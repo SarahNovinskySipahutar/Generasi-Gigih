0f50d9e37769b936409d25b6706a043849c011ea
Sarah1911 <101872543+SarahNovinskySipahutar@users.noreply.github.com>
2022-04-07 00:31:03 +0700

    Module 4 Session 1

---
+++ src/components/playlistCard/playlistCard.js
@@ -0,0 +1,15 @@
+import "./PlaylistCard.css";
+
+function PlaylistCard(props) {
+  return (
+    <div className="Card">
+      <img src={props.url} alt={props.alt}></img>
+      <div className="text-component">
+        <p className="album">{props.albumName}</p>
+        <p>{props.artistName}</p>
+      </div>
+    </div>
+  );
+}
+
+export default PlaylistCard;
---
+++ src/searchForm/SearchForm.js
@@ -0,0 +1,15 @@
+import '../searchForm/SearchForm.css'
+
+const SearchForm = ({ onSearch, handleChange, handlePlaylist }) => {
+  return (
+    <form onSubmit={onSearch}>
+      <input type="text" id="inpuText" onChange={handleChange} required/>
+      <button className='buttonSearch' type="submit" value="submit">
+        Search
+      </button>
+
+    </form>
+  );
+};
+
+export default SearchForm;
